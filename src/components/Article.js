6607ebedf1f9985b3bcf9fbc9002cad1fabc8e05
Archana <potnis.archana@gmail.com>
2019-05-25 20:37:36 -0700

    Removed old code

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,6 @@
 import React from "react";
 import './Article.css'
 
-const style = {
-  color: "red",
-  backgroundColor: "yellow"
-};
 function Article(props) {
   return   <article class="center mw5 mw6-ns hidden ba mv4">
             <h1 class="f4 bg-near-black white mv0 pv2 ph3">Name</h1>
